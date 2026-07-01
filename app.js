72a80c37389ab4cbab1f28de1c72b72e7051a43e
Arik Perera <arik.perera@kaltura.com>
2026-07-02 00:21:57 +0300

    fix: remove accidental yes prefix from line 1 of app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-yes const PROXY_BASE = 'https://pm-proxy.demo.qa.kaltura.ai';
+const PROXY_BASE = 'https://pm-proxy.demo.qa.kaltura.ai';
 const KV_SECRET = 'HPZTjoBph4Cz9AMGwiSsYcJf086bdgRX';
 const APP_VERSION = '1.5.0';
 const CHANGELOG = [
